05179616b0367bee21db59b1be444e48905d497d
frank <Frank.Endriss@bancos.com>
2016-05-29 18:46:09 +0200

    more tries wich MVC, does not work either

--- src/main/java/de/direktbewerten/web/ui/SearchController.java
+++ src/main/java/de/direktbewerten/web/ui/SearchController.java
@@ -1,19 +1,22 @@
 package de.direktbewerten.web.ui;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-//@Controller
+@Controller
 @RequestMapping(value = "/")
 public class SearchController {
 
 	/** The landing page.
-	 * @return index.jsp
+	 * @return index.html
 	 */
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public ModelAndView index() {
-		return new ModelAndView("index.html");
+	@RequestMapping(value="/index.html", method=RequestMethod.GET)
+	public String index(Model model) {
+		System.out.println(""+getClass().getName()+".index() was called");
+		model.addAttribute("username", "Frank");
+		return "index";
 	}
 
 }
