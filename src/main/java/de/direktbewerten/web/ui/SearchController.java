c0e2f77ac01602d13528ca6c52b8d96c9b8118ba
Frank Endriss <fendriss@fum.de>
2016-05-31 08:31:20 +0200

    Added custom error page, 404.html Next: need to make static resources available

--- src/main/java/de/direktbewerten/web/ui/SearchController.java
+++ src/main/java/de/direktbewerten/web/ui/SearchController.java
@@ -1,7 +1,7 @@
 package de.direktbewerten.web.ui;
 
 import org.apache.log4j.Logger;
-
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/")
-public class SearchController {
+public class SearchController implements ErrorController {
 	private final static Logger log=Logger.getLogger(SearchController.class);
 
 	/** The landing page.
@@ -20,6 +20,17 @@ public class SearchController {
 		log.info(".index() was called");
 		model.addAttribute("username", "Frank");
 		return "index";
+	}
+
+	@RequestMapping(value="/error", method=RequestMethod.GET)
+	public String show_404(Model model) {
+		log.info("show_404() was called");
+		return "404";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
 	}
 
 }
